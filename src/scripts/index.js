71b5f93b375cc122a4e1284c5e411530d0087cec
Yazn <36912068+YAZ218@users.noreply.github.com>
2020-05-16 14:32:05 -0700

    Added total confirmed and recovered cases

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,69 +1,70 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkift');
-
-
 var locationsJSON = require('./locations.json');
 
-// Gen random data
 const N = 300;
 let gData = [];
 
-// lat: (Math.random() - 0.5) * 180,
-// lng: (Math.random() - 0.5) * 360,
-// size: Math.random() / 3,
-// color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
 const weightColor = d3.scaleLinear()
-.domain([0, 60])
-.range(['lightblue', 'darkred'])
-.clamp(true);
+    .domain([0, 60])
+    .range(['lightblue', 'darkred'])
+    .clamp(true);
 
 fetch("https://pomber.github.io/covid19/timeseries.json")
-.then(response => response.json())
-.then(data => {
-    for(let key of Object.keys(data)){
-        let country_info = locationsJSON.ref_country_codes.filter((item) => {
-            if(item.country == key){
-                return true;
+    .then(response => response.json())
+    .then(data => {
+        let totalCases = 0;
+        let totalRecovered = 0;
+        for (let key of Object.keys(data)) {
+            let country_info = locationsJSON.ref_country_codes.filter((item) => {
+                if (item.country == key) {
+                    return true;
+                }
+            });
+            if (country_info.length == 0) {
+                continue;
             }
-        }); 
 
-        if(country_info.length == 0){
-            console.log(key);
-            continue;
+            const totals = data[key].pop();
+
+            for(let i = 0; i < data[key].length; i++)
+            {
+                totalCases += data[key].pop().confirmed;
+            }
+
+            for(let i = 0; i < data[key].length; i++)
+            {
+                totalRecovered += data[key].pop().recovered;
+            }
+
+
+            document.getElementById('total-cases').innerHTML = totalCases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");;
+            document.getElementById('total-recovered').innerHTML = totalRecovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+            gData.push({
+                lat: country_info[0].latitude,
+                lng: country_info[0].longitude,
+                size: Math.log(totals.confirmed) / 10,
+                color: weightColor(totals.confirmed),
+                names: country_info[0].country,
+                totals: 'Confirmed: ' + totals.confirmed + '<br/>Deaths: ' + totals.deaths + '<br/>Recovered: ' + totals.recovered
+            });
         }
 
-        const totals = data[key].pop();
+        const world =
+            Globe()
+                .globeImageUrl('//cdn.jsdelivr.net/npm/three-globe/example/img/earth-night.jpg')
+                .labelsData(gData)
+                .labelText('names')
+                .labelLabel('totals')
+                .labelSize('size')
+                .labelColor('color')
+                .labelDotRadius('size')
+                .labelAltitude(0.01)
+                (document.getElementById('globeViz'));
+                
+        world.controls().autoRotate = true;
+        world.controls().autoRotateSpeed = 1;
 
-        gData.push({
-            lat: country_info[0].latitude,
-            lng: country_info[0].longitude,
-            size: Math.log(totals.confirmed) / 10,
-            color: weightColor(totals.confirmed),
-            names: country_info[0].country,
-            totals: 'Confirmed: ' + totals.confirmed + '<br/>Deaths: ' + totals.deaths + '<br/>Recovered: ' + totals.recovered
-        });
-    }
-
-
-const world = 
-
-    Globe()
-    .globeImageUrl('//cdn.jsdelivr.net/npm/three-globe/example/img/earth-night.jpg')
-    .labelsData(gData)
-    .labelText('names')
-    .labelLabel('totals')
-    .labelSize('size')
-    .labelColor('color')
-    .labelDotRadius('size')
-    .labelAltitude(0.01)
-    (document.getElementById('globeViz'));
-
-world.controls().autoRotate = true;
-world.controls().autoRotateSpeed = 1;
-
-});
-
-
-
+    });
 
