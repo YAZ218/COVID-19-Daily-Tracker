b732447c79868cbaa6163b8172132dd8b1a14c1c
Yazn <36912068+YAZ218@users.noreply.github.com>
2020-05-16 18:30:57 -0700

    Totals are updated with accurate data, will be updating source for each countries' individual cases

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,11 +10,24 @@ const weightColor = d3.scaleLinear()
     .range(['lightblue', 'darkred'])
     .clamp(true);
 
-fetch("https://pomber.github.io/covid19/timeseries.json")
+fetch("https://api.thevirustracker.com/free-api?global=stats")
     .then(response => response.json())
     .then(data => {
         let totalCases = 0;
         let totalRecovered = 0;
+
+        for (let key of Object.keys(data)) {
+            totalCases = data[key][0].total_cases;
+            totalRecovered = data[key][0].total_recovered;
+
+            document.getElementById('total-cases').innerHTML = totalCases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            document.getElementById('total-recovered').innerHTML = totalRecovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        }
+    });
+
+fetch("https://pomber.github.io/covid19/timeseries.json")
+    .then(response => response.json())
+    .then(data => {
 
         for (let key of Object.keys(data)) {
             let country_info = locationsJSON.ref_country_codes.filter((item) => {
@@ -22,24 +35,13 @@ fetch("https://pomber.github.io/covid19/timeseries.json")
                     return true;
                 }
             });
+
             if (country_info.length == 0) {
                 continue;
             }
 
             const totals = data[key].pop();
 
-            for(let i = 0; i < data[key].length; i++)
-            {
-                totalCases += data[key].pop().confirmed;
-            }
-
-            for(let i = 0; i < data[key].length; i++)
-            {
-                totalRecovered += data[key].pop().recovered;
-            }
-
-            document.getElementById('total-cases').innerHTML = totalCases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");;
-            document.getElementById('total-recovered').innerHTML = totalRecovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
             gData.push({
                 lat: country_info[0].latitude,
                 lng: country_info[0].longitude,
